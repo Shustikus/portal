b9d129fc52749237193899697bbcde40c5a8c35d
Shustikus <shustikus5@gmail.com>
2025-03-06 12:33:42 +0300

    Добавлена новая акция

--- specialsData.js
+++ specialsData.js
@@ -1,88 +1,88 @@
 // specialsData.js
 
 const specialsData = [
-    {
-        title: "Программа \"Плати раз в год\"",
-        imageUrl: "/images/fecfa66506f108908b33c599189c5f5b.png",
-        link: "programma-plati-raz-v-god",
-        text: " <p class=\"has-factoid\">\n" +
-            " <span class=\"factoid\">сниженный аванс от&nbsp;<span class=\"h1\" style=\"color: #d0043c;\">5%</span></span>\n" +
-            "</p>\n" +
-            "<p style=\"text-align: center;\">\n" +
-            " <img src=\"/images/fecfa66506f108908b33c599189c5f5b.png\">\n" +
-            "</p>\n" +
-            "<p class=\"has-factoid\">\n" +
-            " <span class=\"factoid\">срок лизинга до, месяцев&nbsp;&nbsp;<span class=\"h1\">60</span></span>\n" +
-            "</p>\n" +
-            "<p>\n" +
-            "</p>\n" +
-            "<p>\n" +
-            "\t Компания «Интерлизинг» предлагает специальную программу на технику Ростсельмаш со сниженным авансом от 5%.&nbsp;&nbsp;\n" +
-            "</p>\n" +
-            "<p>\n" +
-            "</p>\n" +
-            " <!--<p class=\"has-factoid\">\n" +
-            " <span class=\"factoid\">срок договора до<span class=\"h1\">8</span> лет</span>\n" +
-            "</p>-->\n" +
-            "<ul type=\"disc\">\n" +
-            "\t<li>Аванс от 5%<br>\n" +
-            " </li>\n" +
-            "\t<li>Программа «Плати раз в год»<br>\n" +
-            " </li>\n" +
-            "\t<li>Сезонные графики платежей<br>\n" +
-            " </li>\n" +
-            "\t<li>Срок лизинга до 60 месяцев.</li>\n" +
-            "</ul>\n" +
-            "<p>\n" +
-            "</p>\n" +
-            "<p>\n" +
-            "\t Выбирайте удобный для вас график платежей: периодичность оплаты и количество платежей вы можете выбрать в зависимости от потребностей вашего бизнеса. Возможен лизинг без оценки финансового состояния! Для получения подробной информации обращайтесь к клиентским менеджерам компании «Интерлизинг».\n" +
-            "</p>",
-    },
-     {
-				title: "Специальное предложение «Оттепель» от АО «Росагролизинг»",
-				imageUrl: "/images/1e753329-e416-495f-8341-26e8dd523fce.jpg",
-				link: "predlozhenie-ottepel",
-				text: " <p class=\"has-factoid\">\n" +
-					" <span class=\"factoid\">авансовый платеж от&nbsp;<span class=\"h1\" style=\"color: #d0043c;\">0%</span></span>\n" +
-					"</p>\n" +
-					"<p style=\"text-align: center;\">\n" +
-					" <img src=\"/images/1e753329-e416-495f-8341-26e8dd523fce.jpg\">\n" +
-					"</p>\n" +
-					"<p class=\"has-factoid\">\n" +
-					" <span class=\"factoid\">срок лизинга до, лет&nbsp;&nbsp;<span class=\"h1\">7</span></span>\n" +
-					"</p>\n" +
-					"<p>\n" +
-					"</p>\n" +
-					"<p>\n" +
-					"\t Специальное предложение «Оттепель» от АО «Росагролизинг»  на приобретение сельскохозяйственной техники Ростсельмаш без первоначальных вложений.\n" +
-					"«Получи технику к новому сезону, а плати за лизинг, когда удобно!»: &nbsp;&nbsp;\n" +
-					"</p>\n" +
-					"<p>\n" +
-					"</p>\n" +
-					" <!--<p class=\"has-factoid\">\n" +
-					" <span class=\"factoid\">срок договора до<span class=\"h1\">8</span> лет</span>\n" +
-					"</p>-->\n" +
-					"<ul type=\"disc\">\n" +
-					"\t<li>отсрочка платежей по основному долгу до октября 2025 года (при аннуитетном графике платежей);<br>\n" +
-					" </li>\n" +
-					"\t<li>авансовый платеж от 0%;<br>\n" +
-					" </li>\n" +
-					"\t<li>срок лизинга до 7 лет;<br>\n" +
-					" </li>\n" +
-					"\t<li>удорожание от 6% в год;</li>\n" +
-					"\t<li>гибкие графики платежей: аннуитетный, регрессивный, сезонный, сезонный с индивидуальными настройками.\n" +
-					"\n</li>\n" +
-					"</ul>\n" +
-					"<p>\n" +
-					"</p>\n",
-		},
-    /*  {
-          title: "Техника по цене 2023 года",
-          imageUrl: "/images/1p41bdg1bhnp25j3iulmf1mmtqreguen.jpg",
-          link: "tekhnika-po-tsene-2023-goda",
-          text: "",
-      }, */
-];
+	{
+		title: 'Программа "Плати раз в год"',
+		imageUrl: '/images/fecfa66506f108908b33c599189c5f5b.png',
+		link: 'programma-plati-raz-v-god',
+		text: ' <p class="has-factoid">\n' +
+			' <span class="factoid">сниженный аванс от&nbsp;<span class="h1" style="color: #d0043c;">5%</span></span>\n' +
+			'</p>\n' +
+			'<p style="text-align: center;">\n' +
+			' <img src="/images/fecfa66506f108908b33c599189c5f5b.png">\n' +
+			'</p>\n' +
+			'<p class="has-factoid">\n' +
+			' <span class="factoid">срок лизинга до, месяцев&nbsp;&nbsp;<span class="h1">60</span></span>\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'\t Компания «Интерлизинг» предлагает специальную программу на технику Ростсельмаш со сниженным авансом от 5%.&nbsp;&nbsp;\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'</p>\n' +
+			' <!--<p class="has-factoid">\n' +
+			' <span class="factoid">срок договора до<span class="h1">8</span> лет</span>\n' +
+			'</p>-->\n' +
+			'<ul type="disc">\n' +
+			'\t<li>Аванс от 5%<br>\n' +
+			' </li>\n' +
+			'\t<li>Программа «Плати раз в год»<br>\n' +
+			' </li>\n' +
+			'\t<li>Сезонные графики платежей<br>\n' +
+			' </li>\n' +
+			'\t<li>Срок лизинга до 60 месяцев.</li>\n' +
+			'</ul>\n' +
+			'<p>\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'\t Выбирайте удобный для вас график платежей: периодичность оплаты и количество платежей вы можете выбрать в зависимости от потребностей вашего бизнеса. Возможен лизинг без оценки финансового состояния! Для получения подробной информации обращайтесь к клиентским менеджерам компании «Интерлизинг».\n' +
+			'</p>'
+	},
+	{
+		title: 'Специальное предложение «Оттепель» от АО «Росагролизинг»',
+		imageUrl: '/images/1e753329-e416-495f-8341-26e8dd523fce.jpg',
+		link: 'predlozhenie-ottepel',
+		text: ' <p class="has-factoid">\n' +
+			' <span class="factoid">авансовый платеж от&nbsp;<span class="h1" style="color: #d0043c;">0%</span></span>\n' +
+			'</p>\n' +
+			'<p style="text-align: center;">\n' +
+			' <img src="/images/1e753329-e416-495f-8341-26e8dd523fce.jpg">\n' +
+			'</p>\n' +
+			'<p class="has-factoid">\n' +
+			' <span class="factoid">срок лизинга до, лет&nbsp;&nbsp;<span class="h1">7</span></span>\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'\t Специальное предложение «Оттепель» от АО «Росагролизинг»  на приобретение сельскохозяйственной техники Ростсельмаш без первоначальных вложений.\n' +
+			'«Получи технику к новому сезону, а плати за лизинг, когда удобно!» : &nbsp;&nbsp;\n' +
+			'</p>\n' +
+			'<p>\n' +
+			'</p>\n' +
+			' <!--<p class="has-factoid">\n' +
+			' <span class="factoid">срок договора до<span class="h1">8</span> лет</span>\n' +
+			'</p>-->\n' +
+			'<ul type="disc">\n' +
+			'\t<li>отсрочка платежей по основному долгу до октября 2025 года (при аннуитетном графике платежей);<br>\n' +
+			' </li>\n' +
+			'\t<li>авансовый платеж от 0%;<br>\n' +
+			' </li>\n' +
+			'\t<li>срок лизинга до 7 лет;<br>\n' +
+			' </li>\n' +
+			'\t<li>удорожание от 6% в год;</li>\n' +
+			'\t<li>гибкие графики платежей: аннуитетный, регрессивный, сезонный, сезонный с индивидуальными настройками.\n' +
+			'\n</li>\n' +
+			'</ul>\n' +
+			'<p>\n' +
+			'</p>\n'
+	}
+	/*  {
+				title: "Техника по цене 2023 года",
+				imageUrl: "/images/1p41bdg1bhnp25j3iulmf1mmtqreguen.jpg",
+				link: "tekhnika-po-tsene-2023-goda",
+				text: "",
+		}, */
+]
 
-module.exports = specialsData;
+module.exports = specialsData
